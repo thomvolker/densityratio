1d9d88dd5847ee89f08f177cff0fb4795f28a44c
Thom Volker <t.b.volker@uu.nl>
2023-03-28 20:19:56 +0200

    Simple ulsif implementation

--- R/ulsif.R
+++ R/ulsif.R
@@ -9,25 +9,34 @@
 #' @param lambda \code{NULL} or a scalar value to determine the regularization
 #' imposed on the Gaussian kernel gram matrix of the denominator samples. If
 #' \code{NULL}, \code{lambda} is chosen to be \eqn{\sqrt{N}}.
-#' @importFrom quadprog solve.QP
+#' @param maxcenters Maximum number of Gaussian centers in the kernel gram
+#' matrix. Defaults to all numerator samples.
+#' @param centers Numeric matrix with the same variables as \code{nu} and
+#' \code{de} that are used as Gaussian centers in the kernel Gram matrix. By
+#' default, the matrix \code{nu} is used as the matrix with Gaussian centers.
 #' @export
 #'
-#' @return \code{kmm} returns \code{rhat_de}, the estimated density ratio for
-#' the denominator samples.
+#' @return \code{ulsif} returns \code{rhat}, the estimated density ratio.
 #'
 #' @examples
 #' x <- rnorm(100) |> matrix(100)
 #' y <- rnorm(200, 1, 2) |> matrix(200)
-#' kmm(x, y)
-#' kmm(x, y, sigma = 2, lambda = 2)
+#' ulsif(x, y)
+#' ulsif(x, y, sigma = 2, lambda = 2)
 #'
 
-ulsif <- function(nu, de, sigma = NULL, lambda = NULL, maxcenters = nrow(nu)) {
+ulsif <- function(nu, de, sigma = NULL, lambda = NULL, maxcenters = nrow(nu),
+                  centers = NULL) {
 
   nu <- as.matrix(nu)
   de <- as.matrix(de)
 
-  if (!is.numeric(de) | !is.numeric(nu) | !ncol(de) == ncol(nu)) {
+  n_nu <- nrow(nu)
+  n_de <- nrow(de)
+  p_nu <- ncol(nu)
+  p_de <- ncol(de)
+
+  if (!is.numeric(de) | !is.numeric(nu) | !p_nu == p_de) {
     stop("Arguments de and nu must be matrices with the same variables")
   }
   if (!is.null(sigma)) {
@@ -40,9 +49,31 @@ ulsif <- function(nu, de, sigma = NULL, lambda = NULL, maxcenters = nrow(nu)) {
       stop("Only scalar values for lambda are currently accepted")
     }
   } else {
-    lambda <- sqrt(nrow(nu) + nrow(de))
+    lambda <- sqrt(n_nu + n_de)
   }
-#  if ()
 
-#  Kdede <- kernel_gaussian()
+  if (!is.numeric(maxcenters) | length(maxcenters) > 1) {
+    stop("maxcenters must be a scalar value indicating how many centers are maximally accepted (for computational reasons)")
+  }
+  if (is.null(centers)) {
+    if (maxcenters < nrow(nu)) {
+      centers <- nu[sample(n_nu, maxcenters), ]
+    } else {
+      centers <- nu
+    }
+  } else {
+    centers <- as.matrix(centers)
+    if (!is.numeric(centers) | !ncol(centers) == p_nu) {
+      stop("If centers are provided, they must have the same variables as the numerator samples")
+    }
+  }
+
+ phi_nu <- kernel_gaussian(nu, centers, sigma)
+ phi_de <- kernel_gaussian(de, centers, sigma)
+ Hhat   <- crossprod(phi_de) / n_de
+ one    <- diag(1, maxcenters)
+ hhat   <- colMeans(phi_de)
+
+ theta <- solve(Hhat + lambda * one) %*% hhat
+ theta
 }
