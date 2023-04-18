d2a0397a183e78d22f418f785ee40a8d23481cf9
Thom Volker <t.b.volker@uu.nl>
2023-04-18 21:59:22 +0200

    Add checks for centers and symmetry (i.e., centers = nu)

--- R/checks.R
+++ R/checks.R
@@ -20,3 +20,41 @@ check.lambda <- function(lambda) {
     stop("lambda must be either NULL, a numeric scalar or a numeric vector")
   }
 }
+
+check.centers <- function(nu, centers, ncenters) {
+
+  if (!is.null(centers)) {
+
+    centers <- as.matrix(centers)
+    check.dataform(nu, centers)
+
+    if (!is.null(ncenters)) {
+      if (nrow(centers) > ncenters) {
+        warning("You have specified more centers than you allowed by ncenters, the ncenters parameter is ignored.")
+      }
+      if (nrow(centers) < ncenters) {
+        warning("ncenters > nrow(centers), the numerator samples are used as gaussian centers.")
+      }
+    }
+  }
+  if (is.null(centers) & (is.null(ncenters))) {
+    centers <- nu
+  }
+  if (is.null(centers) & !is.null(ncenters)) {
+    if (!is.numeric(ncenters) | length(ncenters) > 1) {
+      stop("If ncenters is specified, it must be a scalar value.")
+    } else {
+      centers <- nu[sample(nrow(nu), ncenters), , drop = FALSE]
+    }
+  }
+
+  centers
+}
+
+check.symmetric <- function(centers, ncenters) {
+  if (is.null(centers) & (is.null(ncenters) | ncenters > nrow(centers))) {
+    TRUE
+  } else {
+    FALSE
+  }
+}
