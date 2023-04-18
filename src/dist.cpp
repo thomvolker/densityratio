9392e24dee5e3b2b0369ffaf724cbaa827807fcd
Thom Volker <t.b.volker@uu.nl>
2023-04-18 21:02:57 +0200

    Add new distance function that handles symmetry internally

---
+++ src/dist.cpp
@@ -0,0 +1,49 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+using namespace arma;
+
+
+//' Create a Gram matrix with squared Euclidean distances between
+//' observations in the input matrix \code{X} and the input matrix \code{Y}
+//' @param X A numeric input matrix
+//' @param Y A numeric input matrix with the same variables as x
+
+// [[Rcpp::export]]
+arma::mat distmat(arma::mat X, arma::mat Y, bool symmetric = false) {
+  double dev, dist;
+  int nrx = X.n_rows;
+  int nry = Y.n_rows;
+  int nc = X.n_cols;
+
+  arma::mat out(nrx, nry);
+
+  if (symmetric) {
+    for (int i = 0; i < nrx; i++) {
+      for(int j = i + 1; j < nry; j++) {
+        dist = 0;
+        for(int c = 0; c < nc; c++) {
+          dev = X(i,c) - Y(j,c);
+          dist += dev*dev;
+        }
+        out(i,j) = dist;
+      }
+    }
+    out = out + out.t();
+  } else {
+    for(int i = 0; i < nrx; i++) {
+      for(int j = 0; j < nry; j++) {
+        dist = 0;
+        for (int c = 0; c < nc; c++) {
+          dev = X(i,c) - Y(j,c);
+          dist += dev*dev;
+        }
+        out(i,j) = dist;
+      }
+    }
+  }
+  return out;
+}
+
+
+
+
