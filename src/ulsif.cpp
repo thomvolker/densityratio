ad525804f939543f9687f0d46e4620e148f66989
Thom Volker <t.b.volker@uu.nl>
2023-05-03 23:07:17 +0200

    Adapt compute_ulsif function to easily incorporate cross-validation

--- src/ulsif.cpp
+++ src/ulsif.cpp
@@ -1,13 +1,17 @@
 #include <RcppArmadillo.h>
 #include <omp.h>
+#include "densityratio.h"
 using namespace Rcpp;
 using namespace arma;
 
-arma::vec compute_alpha(arma::mat Hhat, arma::vec hhat, arma::mat diagmat, double lambda) {
-  arma::vec alpha = solve(Hhat + lambda * diagmat, hhat);
+//[[Rcpp::export]]
+arma::vec compute_alpha(arma::mat Hhat, const arma::vec& hhat, const double lambda) {
+  Hhat.diag() += lambda;
+  arma::vec alpha = arma::solve(Hhat, hhat);
   return alpha;
 }
 
+//[[Rcpp::export]]
 int set_threads(int nthreads) {
   int max_threads = omp_get_max_threads();
   if (nthreads == 0) {
@@ -22,19 +26,52 @@ int set_threads(int nthreads) {
 }
 
 //[[Rcpp::export]]
-arma::mat compute_ulsif(arma::mat &Hhat, arma::vec &hhat, arma::vec &lambda, bool parallel, int nthreads) {
+arma::mat make_Hhat(arma::mat dist_de, double sigma) {
+  int n_de = dist_de.n_rows;
+  arma::mat phi_de = kernel_gaussian(dist_de, sigma);
+  arma::mat Hhat = phi_de.t() * phi_de / n_de;
+  return Hhat;
+}
+
+//[[Rcpp::export]]
+arma::mat make_hhat(arma::mat dist_nu, double sigma) {
+  arma::mat phi_nu = kernel_gaussian(dist_nu, sigma);
+  arma::mat hhat = arma::mean(phi_nu, 0);
+  return hhat.t();
+}
+
+//[[Rcpp::export]]
+List compute_ulsif(arma::mat dist_nu, arma::mat dist_de, arma::vec sigma, arma::vec lambda, bool parallel, int nthreads) {
+
+  arma::vec s = make_sigma(dist_nu, sigma);
+  double si;
+  int nsigma  = s.size();
   int nlambda = lambda.size();
-  int p = Hhat.n_cols;
-  arma::mat diagmat = arma::eye<arma::mat>(p, p);
-  arma::mat alpha = arma::zeros<arma::mat>(p, nlambda);
+  int nc = dist_nu.n_cols;
+  int sig, l;
+
+  arma::mat diagmat = arma::eye<arma::mat>(nc, nc);
+  arma::mat Hhat = arma::zeros<arma::mat>(nc, nc);
+  arma::mat hhat = arma::zeros<arma::mat>(nc);
+  arma::cube alpha(nc, nlambda, nsigma, arma::fill::zeros);
 
   if (parallel) {
     nthreads = set_threads(nthreads);
   }
-  #pragma omp parallel for num_threads(nthreads) if (parallel)
-  for(int i = 0; i < nlambda; i++) {
-    alpha.col(i) = compute_alpha(Hhat, hhat, diagmat, lambda(i));
+  for(sig = 0; sig < nsigma; sig++) {
+    si = s[sig];
+    Hhat = make_Hhat(dist_de, si);
+    hhat = make_hhat(dist_nu, si);
+#pragma omp parallel for num_threads(nthreads) if (parallel)
+    for(l = 0; l < nlambda; l++) {
+      alpha.slice(sig).col(l) = compute_alpha(Hhat, hhat, lambda(l));
+    }
   }
-  return alpha;
+  List out = List::create(
+    Named("alpha") = alpha,
+    Named("lambda") = lambda,
+    Named("sigma") = s
+  );
+  return out;
 }
 
