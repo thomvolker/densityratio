25f32e8ff77b7d5c029d0f43a29c9c1618fe7dc3
Thom Volker <t.b.volker@uu.nl>
2023-05-03 23:06:02 +0200

    Add make sigma function and make gaussian kernel function to C++

--- src/dist.cpp
+++ src/dist.cpp
@@ -45,6 +45,28 @@ arma::mat distance(arma::mat X, arma::mat Y, bool symmetric = false) {
   return out;
 }
 
+arma::vec make_sigma(arma::mat dist, arma::vec sigma) {
+
+  arma::vec distvec = arma::vectorise(dist);
+  arma::vec P, Q, s;
+
+  if (sigma(0) == 0) {
+    s = arma::median(distvec);
+  } else if (sigma(0) == -1) {
+    P = {0.25, 0.3, 0.35, 0.4, 0.45, 0.475, 0.5, 0.525, 0.55, 0.6, 0.65, 0.7, 0.75};
+    Q = quantile(distvec, P);
+    s = unique(Q);
+  } else {
+    s = sigma;
+  }
+  return s;
+}
+
+arma::mat kernel_gaussian(arma::mat dist, double sigma) {
+  arma::mat KGM = exp(-dist / (2*sigma*sigma));
+  return KGM;
+}
+
 
 
 
