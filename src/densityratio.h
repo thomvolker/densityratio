5ab5d272718a69a9452978709d7c29190099035c
Thom Volker <t.b.volker@uu.nl>
2023-06-06 14:44:27 +0200

    Remove make_sigma function

--- src/densityratio.h
+++ src/densityratio.h
@@ -3,7 +3,6 @@
 #define DENSITYRATIO
 
 arma::mat distance(arma::mat X, arma::mat Y, bool symmetric = false);
-arma::vec make_sigma(arma::mat dist, arma::vec sigma);
 arma::mat kernel_gaussian(arma::mat dist, double sigma);
 
 #endif
