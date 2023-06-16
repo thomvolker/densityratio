1541dd29793ed35e1ca20d7170b2d68244400ddd
Thom Volker <t.b.volker@uu.nl>
2023-06-16 08:45:46 +0200

    Move computations to C++ and add checks and cross-validation

--- R/kliep.R
+++ R/kliep.R
@@ -1,104 +1,82 @@
 #' Kullback-Leibler importance estimation procedure
 #'
-#' @param nu Numeric matrix with numerator samples
-#' @param de Numeric matrix with denominator samples (must have the same
-#' variables as \code{nu})
+#' @param df_numerator \code{data.frame} with exclusively numeric variables with
+#' the numerator samples
+#' @param df_denominator \code{data.frame} with exclusively numeric variables
+#' with the denominator samples (must have the same variables as
+#' \code{df_denominator})
+#' @param nsigma Integer indicating the number of sigma values (bandwidth
+#' parameter of the Gaussian kernel gram matrix) to use in cross-validation.
+#' @param sigma_quantile \code{NULL} or numeric vector with probabilities to
+#' calculate the quantiles of the distance matrix to obtain sigma values. If
+#' \code{NULL}, \code{nsigma} values between \code{0.25} and \code{0.75} are
+#' used.
 #' @param sigma \code{NULL} or a scalar value to determine the bandwidth of the
-#' Gaussian kernel gram matrix. If \code{NULL}, sigma is the median Euclidean
-#' interpoint distance.
+#' Gaussian kernel gram matrix. If \code{NULL}, \code{nsigma} values between
+#' \code{0.25} and \code{0.75} are used.
 #' @param ncenters Maximum number of Gaussian centers in the kernel gram
 #' matrix. Defaults to all numerator samples.
 #' @param centers Option to specify the Gaussian samples manually.
-#' @param eps Learning rate for the optimization procedure (can be a vector)
+#' @param cv Logical indicating whether or not to do cross-validation
+#' @param nfold Number of cross-validation folds used in order to calculate the
+#' optimal \code{sigma} value (default is 5-fold cv).
+#' @param epsilon Numeric scalar or vector with the learning rate for the
+#' gradient-ascent procedure. If a vector, all values are used as the learning
+#' rate. By default, \code{10^{3:-3}} is used.
 #' @param maxit Maximum number of iterations for the optimization scheme.
-#' @param printFlag Whether or not to print the progress of the optimization
-#' scheme.
+#' @param progressbar Logical indicating whether or not to display a progressbar.
 #' @export
 #'
-#' @return \code{kliep} returns \code{rhat_de}, the estimated density ratio for
-#' the denominator samples.
+#' @return \code{dratio}-object, containing all information to calculate the
+#' density ratio using optimal sigma and optimal weights.
 #'
 #' @examples
 #' set.seed(1)
 #' x <- rnorm(100) |> matrix(100)
 #' y <- rnorm(200, 1, 2) |> matrix(200)
 #' kliep(x, y)
-#' kliep(x, y, sigma = 2)
+#' kliep(x, y, nsigma = 20, ncenters = 100, nfold = 20, epsilon = 10^{3:-5}, maxit = 1000)
 #'
 
-kliep <- function(nu, de, sigma = NULL, ncenters = nrow(nu), centers = NULL,
-                  eps = 0.001, maxit = 100, printFlag = T) {
+kliep <- function(df_numerator, df_denominator, nsigma = 10, sigma_quantile = NULL, sigma = NULL,
+                  ncenters = 200, centers = NULL, cv = TRUE, nfold = NULL,
+                  epsilon = NULL, maxit = 100, progressbar = TRUE) {
 
-  nu <- as.matrix(nu)
-  de <- as.matrix(de)
+  cl <- match.call()
+  nu <- as.matrix(df_numerator)
+  de <- as.matrix(df_denominator)
 
-  n_nu <- nrow(nu)
-  n_de <- nrow(de)
-  p    <- ncol(nu)
+  nnu <- nrow(nu)
 
-  if (!is.numeric(de) | !is.numeric(nu) | ! p == ncol(de)) {
-    stop("Arguments de and nu must be matrices with the same variables")
-  }
-  if (!is.null(sigma)) {
-    if (!is.numeric(sigma) | length(sigma) > 1) {
-      stop("If provided, sigma must be a scalar")
-    }
-  }
-  if (!is.numeric(ncenters) | length(ncenters) > 1) {
-    stop("ncenters must be a scalar value indicating how many centers are maximally accepted (for computational reasons)")
-  }
-  if (is.null(centers)) {
-    if (ncenters < n_nu) {
-      centers <- nu[sample(n_nu, ncenters), ]
-    } else {
-      centers <- nu
-    }
-  } else {
-    centers <- as.matrix(centers)
-    ncenters <- nrow(centers)
-    if (!is.numeric(centers) | ! p == ncol(centers)) {
-      stop("If centers are provided, they must have the same variables as the numerator samples")
-    }
-  }
+  check.dataform(nu, de)
+  centers   <- check.centers(nu, centers, ncenters)
+  symmetric <- check.symmetric(nu, centers)
 
-  Phi <- kernel_gaussian(distance(nu, centers), sigma) |> t()
-  phibar <- kernel_gaussian(distance(de, centers), sigma) |> colMeans()
-  phibar_cp_phibar_inv <- phibar / c(crossprod(phibar))
+  dist_nu <- distance(nu, centers, symmetric)
+  dist_de <- distance(de, centers)
 
-  theta <- rep(1, ncenters)
-  theta <- .impose_constraints(theta, phibar, phibar_cp_phibar_inv)
-  score <- mean(log(crossprod(Phi, theta)))
+  sigma   <- check.sigma(nsigma, sigma_quantile, sigma, dist_de)
+  epsilon <- check.epsilon(epsilon)
+  maxit   <- check.maxit(maxit)
+  cv_ind  <- check.nfold(cv, nfold, sigma, nnu)
 
-  conv <- FALSE
-  iter <- 0
+  out <- compute_kliep(dist_nu, dist_de, sigma, epsilon, maxit, cv_ind, progressbar)
 
-  for (e in eps) {
-    conv <- FALSE
-    iter <- 0
 
-    while (!conv) {
-      iter <- iter+1
-      if(printFlag) cat(paste0("\r Iteration: ", iter))
-      t_temp  <- .gradient_ascent(theta, Phi, e)
-      t_new   <- .impose_constraints(t_temp, phibar, phibar_cp_phibar_inv)
-      s_new   <- mean(log(crossprod(Phi, t_new)))
-      if (s_new - score <= 0 | iter == maxit) {
-        conv <- TRUE
-      } else {
-        score <- s_new
-        theta <- t_new
-      }
-    }
-  }
-  theta
-}
-
-.gradient_ascent <- function(theta, Phi, eps) {
-  theta + eps * crossprod(Phi, 1 / Phi %*% theta)
-}
-
-.impose_constraints <- function(theta, phibar, phibar_cp_phibar_inv) {
-  theta <- theta + phibar_cp_phibar_inv * c(1 - crossprod(phibar, theta))
-  theta <- pmax(0, theta)
-  theta %*% (1 / crossprod(phibar, theta))
+  out <- list(
+    df_numerator = df_numerator,
+    df_denominator = df_denominator,
+    alpha = out$alpha,
+    cv_score = switch(cv, out$cv_score, NULL),
+    sigma = sigma,
+    centers = centers,
+    nfold = switch(cv, max(cv_ind) + 1, NULL),
+    epsilon = epsilon,
+    maxit = maxit,
+    alpha_opt = switch(cv, out$alpha[, which.max(out$cv_score), drop = FALSE], NULL),
+    sigma_opt = switch(cv, sigma[which.max(out$cv_score)], NULL),
+    call = cl
+  )
+  class(out) <- "kliep"
+  out
 }
