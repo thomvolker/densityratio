fc99b5b58890830045a74cd9f5e2b9424e81dd00
Thom Volker <t.b.volker@uu.nl>
2023-06-25 11:28:06 +0200

    Add summary print method for ulsif

--- R/print.R
+++ R/print.R
@@ -1,29 +1,60 @@
 #' Print a \code{ulsif} object
 #'
 #' @rdname print
-#' @param object Object of class \code{ulsif} or \code{kliep}
-#' @return \code{NULL}
+#' @param x Object of class \code{ulsif}, \code{summary.ulsif}, \code{kliep}
+#' or \code{summary.kliep}.
+#' @param digits Number of digits to use when printing the output.
+#' @param ... further arguments on how to format the number of digits.
+#' @return \code{invisble} The inputted \code{ulsif} object.
 #' @method print ulsif
 #' @importFrom utils str
 #' @export
 
-print.ulsif <- function(object, digits = max(3L, getOption("digits") - 3L), ...) {
-  cat("\nCall:\n", paste0(deparse(object$call)), "\n", sep = "")
+print.ulsif <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
+  cat("\nCall:\n", paste0(deparse(x$call)), "\n", sep = "")
   cat("\n")
   cat("Kernel Information:\n",
       "  Kernel type: Gaussian with L2 norm distances\n",
-      "  Number of kernels: ", paste0(nrow(object$centers)), "\n", sep = "")
+      "  Number of kernels: ", paste0(nrow(x$centers)), "\n", sep = "")
   cat("  sigma:")
-  cat(str(unname(object$sigma)))
+  cat(str(unname(x$sigma)))
   cat("  lambda:")
-  cat(str(unname(object$lambda)))
-  cat("  Optimal sigma: ", paste(format(object$sigma_opt, digits)), "\n",
-      "  Optimal lambda: ", paste(format(object$lambda_opt, digits)), "\n", sep = "")
+  cat(str(unname(x$lambda)))
+  cat("  Optimal sigma: ", paste(format(x$sigma_opt, digits, ...)), "\n",
+      "  Optimal lambda: ", paste(format(x$lambda_opt, digits, ...)), "\n", sep = "")
   cat("  Optimal kernel weights (loocv):")
-  cat(str(object$alpha_opt), "\n")
+  cat(str(x$alpha_opt), "\n")
+  invisible(x)
 }
 
-#' Print a \code{ulsif} object
+#' Print a \code{summary.ulsif} object
+#'
+#' @rdname print
+#' @return \code{NULL}
+#' @method print summary.ulsif
+#' @importFrom utils str
+#' @export
+
+print.summary.ulsif <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
+  cat("\nCall:\n", paste0(deparse(x$call)), "\n", sep = "")
+  cat("\n")
+  cat("Kernel Information:\n",
+      "  Kernel type: Gaussian with L2 norm distances\n",
+      "  Number of kernels: ", paste0(nrow(x$centers)), "\n", sep = "")
+
+  cat("  Optimal sigma: ", paste(format(x$sigma_opt, digits, ...)), "\n",
+      "  Optimal lambda: ", paste(format(x$lambda_opt, digits, ...)), "\n", sep = "")
+  cat("  Optimal kernel weights (loocv):")
+  cat(str(x$alpha_opt), "\n")
+  #TODO: Check pearson divergence interpretation
+  cat("Pearson divergence between P(nu) and P(de): ", paste(format(x$PE, digits = digits, ...)), "\n", sep = "")
+  if (!is.null(x$p_value)) {
+    cat("Pr(P(nu)=P(de)) = ", paste(format(x$p_value, digits = 3, ...)), "\n\n", sep = "")
+  }
+  invisible(x)
+}
+
+#' Print a \code{kliep} object
 #'
 #' @rdname print
 #' @return \code{NULL}
@@ -31,25 +62,25 @@ print.ulsif <- function(object, digits = max(3L, getOption("digits") - 3L), ...)
 #' @importFrom utils str
 #' @export
 
-print.kliep <- function(object, digits = max(3L, getOption("digits") - 3L), ...) {
-  cat("\nCall:\n", paste0(deparse(object$call)), "\n", sep = "")
+print.kliep <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
+  cat("\nCall:\n", paste0(deparse(x$call)), "\n", sep = "")
   cat("\n")
   cat("Kernel Information:\n",
       "  Kernel type: Gaussian with L2 norm distances\n",
-      "  Number of kernels: ", paste0(nrow(object$centers)), "\n", sep = "")
+      "  Number of kernels: ", paste0(nrow(x$centers)), "\n", sep = "")
   cat("  sigma:")
-  cat(str(unname(object$sigma)))
-  if (!is.null(object$cv_score)) {
-    cat("  Optimal sigma (", paste(object$nfold), "-fold cv): ", paste(format(object$sigma_opt, digits = digits), collapse = "  "), "\n", sep = "")
-    cat("  Optimal kernel weights (", paste(object$nfold), "-fold cv): ", sep = "")
-    cat(str(object$alpha_opt))
+  cat(str(unname(x$sigma)))
+  if (!is.null(x$cv_score)) {
+    cat("  Optimal sigma (", paste(x$nfold), "-fold cv): ", paste(format(x$sigma_opt, digits = digits, ...), collapse = "  "), "\n", sep = "")
+    cat("  Optimal kernel weights (", paste(x$nfold), "-fold cv): ", sep = "")
+    cat(str(x$alpha_opt))
   } else {
     cat("  Optimal sigma: NULL (no cross-validation)\n", sep = "")
     cat("  Optimal kernel weights: NULL (no cross-validation)\n", sep = "")
   }
   cat("\nOptimization parameters:\n", sep = "")
-  cat("  Learning rate (epsilon): ", paste(format(object$epsilon, digits = digits), collapse = "  "), "\n", sep = "")
-  cat("  Maximum number of iterations: ", paste(object$maxit, collapse = "  "))
+  cat("  Learning rate (epsilon): ", paste(format(x$epsilon, digits = digits, ...), collapse = "  "), "\n", sep = "")
+  cat("  Maximum number of iterations: ", paste(x$maxit, collapse = "  "))
   cat("\n")
-  invisible(object)
+  invisible(x)
 }
