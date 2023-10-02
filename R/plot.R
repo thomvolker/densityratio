3f0752edefa8cb7e0e43055ab73eb87f110c89df
CarlosPoses <carlosgonzalezposes@gmail.com>
2023-10-02 15:49:36 +0200

    separete plot functions
    Separetes plot functions into three functions
    1. Default: Plots density ratio histograms of both samples
    2. Univariate: Plot density ratio value against value of one variable.
    3. Bivariate: Plot density ratio value (in colors), for combinations of two variables

--- R/plot.R
+++ R/plot.R
@@ -1,58 +1,114 @@
 
-plot.ulsif <- function(object, type = "univariate", var1 = NULL, var2, ...) {
+plot.ulsif <- function(object, sample = "both") {
+
+  data <- rbind(object$df_numerator, object$df_denominator, make.row.names = TRUE)
+  data$dr <- predict(object, newdata = data)
+
+  data$sample[1:nrow(object$df_numerator)] <- "Numerator"
+  data$sample[(nrow(object$df_numerator) + 1):nrow(data)] <- "Denominator"
+
+  if(sample == "numerator"){
+    data <- data %>% filter(sample == "Numerator")
+  }
+  if(sample == "denominator"){
+    data <- data %>% filter(sample == "Denominator")
+  }
+  if(sample == "both"){
+    data <- data
+  }
+
+  plot <-
+    ggplot(data, aes(x = dr)) +
+    geom_histogram(aes(fill = sample), alpha = .75,
+                   color = "black",
+                   position = "identity",
+                   bins = max(data$dr)) +
+    scale_x_continuous(breaks = seq(round(min(data$dr), 0),round(max(data$dr), 0))) +
+    scale_fill_manual(values = c("firebrick", "steelblue")) +
+    theme_bw()  +
+    labs(
+      x = "Density ratio",
+      y = "Count"
+    ) +
+    labs(title = "Distribution of density ratio estimates",
+         fill = "Sample")
+
+  return(plot)
+}
+
+
+plot_univariate <- function(object, vars, sample = "both") {
 
   # Data handling
   data <- rbind(object$df_numerator, object$df_denominator)
   data$dr <- predict(object, newdata = data)
 
-  # Univariate plot
+  data$sample[1:nrow(object$df_numerator)] <- "Numerator"
+  data$sample[(nrow(object$df_numerator) + 1):nrow(data)] <- "Denominator"
 
-  if(type == "univariate") {
-    if(is.null(var1)){
-      cor <- cor(data)[1:ncol(data)-1,ncol(data)]
-      var <- names(cor[which.max(cor)])
-    }
-    else {
-      var <- var1
-    }
+  if(sample == "numerator"){
+    data <- data %>% filter(sample == "Numerator")
+  }
+  if(sample == "denominator"){
+    data <- data %>% filter(sample == "Denominator")
+  }
+  if(sample == "both"){
+    data <- data
+  }
 
+
+  plots <- list()
+
+  plot_onevariable <- function(var, shape = "sample"){
     plot <-
-      ggplot(data, aes(x = .data[[var]], y = .data[["dr"]])) +
-      geom_point(aes(col = dr)) +
+      ggplot(data, aes(x = .data[[var]], y = dr )) +
+      geom_point(aes(col = dr, shape = sample)) +
       theme_bw() +
       labs(y = "Density ratio") +
-      labs(title = "Scatter plot of individual values and density ratio") +
-      scale_colour_viridis_c(option = "B", name ="Density ratio")
+      labs(title = "Scatter plot of individual values and density ratio",
+           shape = "Sample") +
+      scale_colour_viridis_c(option = "B", name ="Density ratio")  +
+      scale_shape_manual(values = c(16, 3))
 
+
+    return(plot)
   }
 
-  # Histogram
-  if(type == "histogram") {
-    plot <-
-      ggplot(data, aes(x = dr)) +
-      geom_histogram(fill = "grey25", color = "black",
-                     bins = max(data$dr)) +
-      scale_x_continuous(breaks = seq(round(min(data$dr), 0),round(max(data$dr), 0))) +
-      theme_bw()  +
-      labs(
-        x = "Density ratio",
-        y = "Count"
-      ) +
-      labs(title = "Distribution of density ratio estimates")
+
+  for(var in vars){
+    plots[[var]] <- plot_onevariable(var)
   }
 
-  # Bivariate
-  if(type == "bivariate") {
-    plot <-
-      ggplot(data, mapping = aes(x = .data[[var1]], y = .data[[var2]])) +
-      geom_point(aes(col = dr)) +
-      scale_colour_viridis_c(option = "B", name ="Density ratio") +
-      theme_bw() +
-      labs(x = var1,
-           y = var2) +
-      labs(title = "Density ratio estimates for combinations of values")
+  return(plots)
+}
 
+plot_bivariate <- function(object, vars, sample = "both") {
+
+  data <- rbind(object$df_numerator, object$df_denominator)
+  data$dr <- predict(object, newdata = data)
+
+  data$sample[1:nrow(object$df_numerator)] <- "Numerator"
+  data$sample[(nrow(object$df_numerator) + 1):nrow(data)] <- "Denominator"
+
+  if(sample == "numerator"){
+    data <- data %>% filter(sample == "Numerator")
   }
+  if(sample == "denominator"){
+    data <- data %>% filter(sample == "Denominator")
+  }
+  if(sample == "both"){
+    data <- data
+  }
+
+
+  plot <-
+    ggplot(data, mapping = aes(x = .data[[vars[1]]], y = .data[[vars[2]]])) +
+    geom_point(aes(col = dr, shape = sample)) +
+    scale_colour_viridis_c(option = "B", name ="Density ratio") +
+    theme_bw() +
+    labs(title = "Density ratio estimates for combinations of values",
+         shape = "Sample") +
+    scale_shape_manual(values = c(16, 3))
 
   return(plot)
 }
